9aeca328b6e2f08dcabf3a238dd9edcfb186afc8
Mitch Masterson <mitchell.masterson@gmail.com>
2016-08-07 01:11:50 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,7 +1,11 @@
-var playlist = {artist: 'Song Title'};
+var playlist = {
+  'Josie and the Pussycats': 'Some Song',
+  'Bully': 'Some other Song'
+}
 
 function updatePlaylist(list, artist, title) {
-  return Object.assign( {}, list, {[artist]: title} )
+  list[artist] = title
+  return list
 }
 
 function removeFromPlaylist(list, artistName) {

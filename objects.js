c864d06119ba8f6dc46eafdaf7d83e222dfc46e6
Mort Mackof <mmackof@alum.rpi.edu>
2016-08-13 01:48:53 +0000

    Done.

--- objects.js
+++ objects.js
@@ -6,10 +6,7 @@ function updatePlaylist(playlist,artist,title){
   playlist [artist] = title;
   return playlist
 }
-//artist = "Kanye"
-
   function removeFromPlaylist(playlist,artist){
-
     delete playlist[artist]
     return playlist
 }

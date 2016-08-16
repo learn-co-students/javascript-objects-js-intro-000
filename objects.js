34fc072d83a1a9d5535ced0ae0e6a6221e705fc7
syed shaheeruddin <shaheeruddinsyeds@gmail.com>
2016-08-16 17:06:44 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,8 +1,9 @@
 var playlist={shaheer: "amsterdam", imaginedragons: "hear me"};
 
 function updatePlaylist(playlist, artistName, songTitle){
-  object.assign({}, playlist, { [artistName]: songTitle});
-return playlist
+ playlist{Phil Ochs = "Here's to the State of Mississippi"}
+ return playlist
+  //Object.assign({}, playlist, { [artistName]: songTitle})
 }
 function removeFromPlaylist(playlist, artistName){
   delete playlist.artistName;

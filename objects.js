a845d5bebcbc467258d8d895733d3ce6cedba1a3
jason nguyen <tjasonnguyen@gmail.com>
2016-08-18 21:17:34 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,14 @@
+var playlist = {
+  'Phil Ochs': 'Here\'s to the State of Mississippi',
+  Kayne: 'Gold Digger'
+}
+
+function updatePlaylist (playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist (playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+}

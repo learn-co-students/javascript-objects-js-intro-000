d65cb14ed221ca965d9bce8dfc1134026db96582
Cindy Gao <cindygao@hotmail.com>
2016-08-26 18:37:33 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist= { katy: 'The One That Got away', taylor: 'teardrops on my guitar', aerosmith: 'dont wanna miss a thing'};
+//var playlist = {artistName: songTitle}
+
+function updatePlaylist(playlist, artistName, song
+Title){
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName];
+  return playlist;
+}

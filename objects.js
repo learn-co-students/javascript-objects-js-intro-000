01f87e11f5ea9d97c78952285be271f529573eee
David Ganthier <david.ganthier@gmail.com>
2016-08-28 03:01:52 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,26 @@
+// (1) Object defines an object called `playlist`:
+// (2) The playlist is limited to the artist name, and song titles.
+// (3) Is line 2 true?
+var playlist = {
+  artistName: "Song titles"
+};
+
+/*objects updatePlaylist(playlist, artistName, songTitle) adds the `artistName:
+songTitle` key-value pair to `playlist`:*/
+/* Create a function `updatePlaylist` which accepts three parameters: the
+playlist (an object), an artist name (a string), and a song title. The body of
+the function should add the song and artist as a key-value pair to the playlist
+object. The function should return the whole playlist.*/
+function updatePlaylist(playlist, artistName, songTitle) {
+  return Object.assign({}, playlist, {[artistName]: songTitle});
+}
+
+/*objects removeFromPlaylist(playlist, artistName) removes `artistName` from
+`playlist`:*/
+/*Create a function `removeFromPlaylist` which accepts two arguments
+(the playlist object and the artist name). The body of the function should
+delete the key-value pair from the playlist and return the updated playlist.*/
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.Kanye;
+  return playlist;
+}

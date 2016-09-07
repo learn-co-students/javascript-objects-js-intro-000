1917803e3e0f60c4cad120c8fe6d95b7364d1fc0
William Barela <william.barela.dev@gmail.com>
2016-09-07 16:56:43 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {Jamiroquai: 'Cosmic Girl'};
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName];
+  return playlist;
+}

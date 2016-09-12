b7b8dfb14f6aab8788688aacee069a5661d15678
William Gough <william.a.gough@gmail.com>
2016-09-12 21:05:56 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {song1: "black hole sun"}
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName]
+  return playlist
+}

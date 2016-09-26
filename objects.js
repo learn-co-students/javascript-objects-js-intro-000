ff32376f634836296cf03ee44ee5fd6e1b9b3b51
Tayari Appiah <cjhhh.ta@gmail.com>
2016-09-26 08:54:32 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {Omarion: "Ice-box"};
+
+function updatePlaylist(playlist, artistName, songTitle){
+  return Object.assign({}, playlist, {[artistName]: songTitle })
+}
+
+function removeFromPlaylist(playlist, artistName){
+    delete playlist.Kanye;
+     return playlist;
+
+}

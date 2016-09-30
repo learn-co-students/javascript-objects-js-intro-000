288f2fe1ab4f1749f0c60b1b8979422ce9014eef
Dimitris Papathanasiou <papafingoss@gmail.com>
2016-09-30 18:50:18 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,16 @@
+var playlist={
+  Bdc:'kalinixta',
+  fgt:'sdfew',
+  dre:'asdx',
+  Kanye: 'Gold Digger'
+}
+
+function updatePlaylist(playlist,artistName,songTitle){
+  playlist[artistName]=songTitle;
+  return playlist
+}
+
+function removeFromPlaylist(playlist,artistName){
+  var playlist={};
+  return playlist
+}

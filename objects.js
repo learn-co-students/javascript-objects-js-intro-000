cd408930c8f71d6c44c208886732db89cd0a9c95
MoJen Jenkins <mojen@thenoisemakers.co.uk>
2016-10-11 20:36:06 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {"artistName" : "song"};
+
+function updatePlaylist( playlist, artist, song) {
+  playlist[artist] = song;
+  return playlist;
+}
+
+function removeFromPlaylist( playlist, artistName ) {
+  delete playlist[artistName];
+  return playlist;
+}

f7f4f3ae4d34f7e0821010b318ffdf364806227d
Thomas Hines <thomashines@gmail.com>
2016-10-12 03:53:57 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,16 @@
+var playlist = {
+  //BobDylan: 'X',
+  //BackstreetBoys: 'Y',
+  Kanye: 'Gold Digger'
+}
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName];
+  return playlist;
+}
+//removeFromPlaylist(playlist, 'Kanye');

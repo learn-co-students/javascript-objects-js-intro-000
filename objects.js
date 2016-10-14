988199fcce6c643fff7e86e45c2b27777724163f
Carlo Bruno <cloudcb_11@hotmail.com>
2016-10-14 03:51:43 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {0:1};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.Kanye;
+  return playlist
+}

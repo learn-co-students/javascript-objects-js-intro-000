197744592b73b8312fdc503188205eebec89b904
Samuel Comerford <samcomerford1825@gmail.com>
2016-10-14 17:12:04 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {JamieT: 'Ike and Tina'}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+ playlist[artistName] = songTitle
+ return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist
+}

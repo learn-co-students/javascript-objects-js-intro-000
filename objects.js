ccc26fae87644a59c35b147a1cc832c09573bda0
Adam Ergas <adamergas@yahoo.com>
2016-11-19 05:01:45 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = { names: "titles" };
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName];
+  return playlist;
+}

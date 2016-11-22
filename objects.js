aa1b9b6e2b09a525f94003c3ed1bf7509daed9f0
Cameron Dunne <camdunne@gmail.com>
2016-11-22 21:44:26 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = {
+  'Lady Gaga': 'Just Dance'
+};
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName, songTitle){
+  delete playlist[artistName];
+  return playlist;
+}

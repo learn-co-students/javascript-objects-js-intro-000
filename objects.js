57d1087f28e96de5275e16cf8862d44809dc7d0d
Anna Brakowska <anna.brakowska91@gmail.com>
2016-11-23 04:43:04 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,12 @@
+var playlist = {;
+
+// function updatePlaylist(playlist, "artistName", "songTitle"){
+//   playlist.artistName = "songTitle";
+//   // return updatePlaylist;
+// }
+
+
+//
+// function removeFromPlaylist (object,artistName) {
+//
+// }

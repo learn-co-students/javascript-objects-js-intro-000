b1d845699ffdb4c9a9ab4d848ba7ad95ea39df51
Stuart Hahn <slumpcity@outlook.com>
2016-11-29 03:41:05 +0000

    Done.

--- objects.js
+++ objects.js
@@ -2,12 +2,14 @@ var playlist = {
   artist: 'song titles'
 }
 
-function updatePlaylist(playlist, artist, song) {
-  playlist[artist] = song
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+
   return playlist
 }
 
 function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName]
+
   return playlist
 }

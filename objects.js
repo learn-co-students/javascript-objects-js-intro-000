afe41b00286e4831fa2a415bca8642b2307f7440
Anna Brakowska <anna.brakowska91@gmail.com>
2016-11-30 05:34:21 +0000

    Done.

--- objects.js
+++ objects.js
@@ -2,14 +2,13 @@ var playlist = {"Phil Ochs": "Here's to the State of Mississippi"}
 
 function updatePlaylist(obj, key, value){
   // Object.assign({ "Phili Ochs": "Here's to the State of Mississippi" })
-   Object.assign({},  { "artistName":"songTitle"})
-   return Object.assign(obj, {key: value}
-  //  return playlist;
+  //  Object.assign({},  { artistName:"songTitle"})
+    // return playlist;
+    return Object.assign(obj, {[key]: value})
 }
 
-
-function removeFromPlaylist (obj,key,value) {
-  delete playlist.artistName;
+function removeFromPlaylist (obj,key) {
+  delete playlist["Phil Ochs"];
   return playlist
 
 }

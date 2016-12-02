f011035d2e4e8801388d8625d09cdc0b4991b8cc
Claudia Maciel <clc_80@yahoo.com>
2016-12-02 07:18:12 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {key: "value"};
+
+function updatePlaylist(playlist, artistName, title){
+  playlist[artistName] = title;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName];
+  return playlist;
+}

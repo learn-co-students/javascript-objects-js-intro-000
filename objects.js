9d0020c6aee54ef49da76639f03a7faf0de3a9f2
Aaron R. Trevino <aaronrtrevino12@gmail.com>
2016-12-09 03:08:10 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,16 @@
+var playlist = {
+  Slowdive: 'Alison'    
+}
+
+playlist['Slowdive'] = 'Alison'
+playlist['My Bloody Valentine'] = 'Sometimes'
+
+function updatePlaylist(){
+  playlist['Phil Ochs'] = "Here's to the State of Mississippi"  
+  return playlist
+}
+
+function removeFromPlaylist(){
+  delete playlist.Slowdive
+  return playlist
+}

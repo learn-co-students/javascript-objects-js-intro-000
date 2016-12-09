cb3e0697367f7fcf35b4ca3ecf6e53860aa9505c
Tim Schmidt <schmidt.tim.j@gmail.com>
2016-12-09 01:57:45 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist = {
+  Slowdive: 'Alison',
+  MyBloodyValentine: 'Sometimes',
+};
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist['Slowdive']= 'Alison';
+  playlist['My Bloody Valentine']= 'Sometimes';
+};

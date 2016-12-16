c5a4fd56036547011153d575f261c65bf205c0fe
David Heisner <davidheis@hotmail.com>
2016-12-16 16:58:04 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {Slowdive: 'Aliso'};
+
+function updatePlaylist(playlist, artistName, songTitle){
+    playlist[artistName] = songTitle;
+    return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName){
+    delete playlist[artistName];
+    return playlist;
+}

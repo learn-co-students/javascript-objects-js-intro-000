20fed2d7428cce7c43122f86e6369194da982455
Ronnie Smith <halfpint1170@gmail.com>
2016-12-22 07:27:54 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist = {artistA: 'songA'}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+}

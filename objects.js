7dddce93c3bfab3e10ffbc943a47aa7473a7158a
Salvatore McCarty <salvatore.mccarty@gmail.com>
2016-12-29 17:35:31 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = { Cake: 'Going the Distance' };
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+}

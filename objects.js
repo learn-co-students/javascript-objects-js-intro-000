0f4bca564bd2cbde23cd9a5440ca644ad84f8bd0
Leon Mercanti <digital_domain@hotmail.com>
2017-01-04 14:57:00 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,6 +1,14 @@
-var playlist = {};
+var playlist = {
+  artist:  "Here is my song",
+  address: "12 Beach Front"
+};
 
-function updatePlaylist(playlist, ) {
-  
-}
+function updatePlaylist(obj, name, song) {
+  obj[name] = song;
+  return obj;
+};
 
+function removeFromPlaylist(playlist, name) {
+  delete playlist[name];
+  return playlist;
+};

e01d49efa5cb1e58c40cc924a93adfe56ad87a94
Zabed Al Karim <zabedalkarim@gmail.com>
2017-01-15 21:09:19 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = {
+	artist01 : "songTitles01",
+    artist02 : "songTitles02",
+    artist03 : "songTitles03"
+};
+function updatePlaylist(playlist, key, value) {
+      playlist[key] = value;
+      return playlist;
+};
+

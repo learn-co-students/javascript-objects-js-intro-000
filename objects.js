ae0fbba0988ef0f477233db58cf78acabe8f7ec3
Olivia Hanson <olivia.joy.hanson@gmail.com>
2017-01-20 18:17:41 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = { "LadyGaga" : "Bad Romance" };
+
+function updatePlaylist ( playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist (playlist, artistName) {
+  delete playlist[artistName];
+  return playlist;
+}

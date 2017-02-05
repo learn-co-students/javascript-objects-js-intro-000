0e574a8b08a432acc2cc3e81482b1d67e2989941
rosa lee <rosalee0116@gmail.com>
2017-02-05 00:03:56 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,14 @@
+var playlist = {
+  BlackPink: "Fire",
+  EXO: "Growl"
+  }
+
+function updatePlaylist (playlist, artistName, songTitle){
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist (playlist, artistName){
+  delete playlist[artistName]
+  return playlist
+}

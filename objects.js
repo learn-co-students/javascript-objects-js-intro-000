241d1bb6470aaaa74b1f223b5a7958f7cd171f1b
Michal Vacek <vahyno@gmail.com>
2017-02-28 06:30:48 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = new Object({Deux: 'Carpe Diem'});
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle;
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist.Slowdive//artistName;
+  
+}

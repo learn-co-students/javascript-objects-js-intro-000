cd1b459eb1f9274718840a8f075678b5f04bfd57
Tori Catipon <evgcatipon@gmail.com>
2017-03-02 02:54:28 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {
+  glassAnimals: 'cane shuga'
+}
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle
+  return playlist
+}
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName];
+  return playlist
+}

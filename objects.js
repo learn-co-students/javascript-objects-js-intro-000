9ed56036876d60d7f2fa27eb32a21baea35b7bf4
Kelsie Medina <kels.garvey@gmail.com>
2017-03-03 15:51:19 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {
+  'Phil Ochs': 'My Bloody Valentine'
+};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist ['Slowdive'] = 'songTitle'
+};
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.Slowdive;
+}

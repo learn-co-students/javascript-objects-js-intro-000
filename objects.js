ab6e788308013ab2067fced6401a2ef8b8eecd54
Greg Benjamin <gregbenj@gmail.com>
2017-03-14 02:16:48 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = {"The Black Keys": "Thickfreakness"}
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName]
+}

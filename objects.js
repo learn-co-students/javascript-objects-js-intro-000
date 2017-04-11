d7f288a850ef955c8f1e287b09b008346421292c
Benjamin Soung <soungbenjamin@gmail.com>
2017-04-11 16:06:50 +0000

    passed

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {
+  eminem: 'when im gone'
+}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+}

753e56c206e231f28d861ba22adced91bdd1853f
Alex Glover <Alex@MacBook-Pro-3.local>
2017-04-24 22:16:50 -0400

    Done.

---
+++ objects.js
@@ -0,0 +1,10 @@
+playlist = new Object({Nemes: "Everest Isle"});
+
+function updatePlaylist(playlist, artist, song){
+  return Object.assign({}, playlist, {[artist]: song})
+};
+
+function removeFromPlaylist(playlist,artistName){
+  delete playlist[artistName]
+  return playlist
+};

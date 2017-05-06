16df83c57f591af737443e27c9bad64a0a33f06d
Oscar Pullay <oscar_e1993@hotmail.com>
2017-05-06 17:53:22 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = {artist1:'song1',artist2:'song2'};
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+
+
+function removeFromPlaylist(playlist, artistName){
+delete playlist.Slowdive;
+  return playlist;
+}
+//removeFromPlaylist(playlist, 'Slowdive')

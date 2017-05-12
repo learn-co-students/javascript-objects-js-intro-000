2670b048894bd3088420dde1ea455cf49dda3818
david Lee <davidslee101@gmail.com>
2017-05-12 21:11:12 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = {
+  BenMarshall: 'what do i say'
+};
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = "songTitle";
+  return playlist;
+};
+
+function removeFromPlaylist(playlist,artistName){
+  delete playlist[artistName]
+  return playlist;
+};

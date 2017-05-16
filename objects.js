e3fb7e7705a9923c796509ecff2d7f817c1d2a13
Dustin Anderson <dandersonsoftware@outlook.com>
2017-05-16 15:53:35 +0000

    Done.

--- objects.js
+++ objects.js
@@ -2,7 +2,11 @@ var playlist = {
   DMB: "Crash"
 }
 
-function updatePlaylist(playlist, artist, songTitle) {
-  playlist.artist = songTitle;
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
   return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]; 
 }

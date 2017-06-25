89b9bb18859a7e60098a62da1774e94a12bcddba
Dylan Sims <dsims10@vols.utk.edu>
2017-06-25 17:53:46 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {artistName: 'songTitle'}
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  return Object.assign({}, playlist, artistName, songTitle)
+  return Object.assign({}, artistName, songTitle)
 }
 
 function removeFromPlaylist(playlist, artistName) {

4a7ee2f6ccc0814a1b909a6da715ef67d68cf3b4
Katie Jergens <katie.jergens@gmail.com>
2017-06-29 12:29:28 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,15 @@
+var playlist = {
+  Beyonce: "Irreplaceable"
+}
+
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+
+}

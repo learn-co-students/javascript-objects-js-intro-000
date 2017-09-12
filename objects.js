0c00dba8f6d51111f49fedd4b8edb1a5373ee32a
Dean Norman <dean@22seven.com>
2017-09-12 09:40:57 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = {
+  'Brand New': 'Jesus Christ'
+}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist;
+}

b011a35943faed5ffa5272caf546d6d21e32dd7b
Aaron R. Trevino <aaronrtrevino12@gmail.com>
2017-10-16 11:18:14 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,16 +1,13 @@
 var playlist = {
-  Slowdive: 'Alison'    
+  MercyMe: 'I Can Only Imagine'
 }
 
-playlist['Slowdive'] = 'Alison'
-playlist['My Bloody Valentine'] = 'Sometimes'
-
-function updatePlaylist(){
-  playlist['Phil Ochs'] = "Here's to the State of Mississippi"  
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle
   return playlist
 }
 
-function removeFromPlaylist(){
-  delete playlist.Slowdive
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName]
   return playlist
 }

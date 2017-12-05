37c871627ca3e4effb808cfd0286f21ae28feba3
alec <alec@petros.audio>
2017-12-05 19:47:55 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,7 @@
+var playlist = { lido:"everything" }
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+}
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+}

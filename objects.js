2c58e9cd9ba8884cdb0aef1e7d5d7651fd11c3d8
Mario Rodrigues <mariorodjr92@gmail.com>
2017-12-14 16:56:50 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,15 @@
+var playlist = {
+  newPolitics: 'One of us'
+};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist['Slowdive'] = 'Alison';
+  playlist['My Bloody Valentine'] = 'Sometimes';
+  playlist['Phil Ochs'] = "Here's to the State of Mississippi";
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist['Slowdive'];
+  return playlist
+}

3c457d737f640fb7eecf8c931c752b090e344463
Mario Rodrigues <mariorodjr92@gmail.com>
2017-12-14 16:58:19 +0000

    Done.

--- objects.js
+++ objects.js
@@ -11,5 +11,5 @@ function updatePlaylist(playlist, artistName, songTitle) {
 
 function removeFromPlaylist(playlist, artistName) {
   delete playlist['Slowdive'];
-  return playlist
+  return playlist;
 }

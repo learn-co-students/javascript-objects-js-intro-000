845e148d2cb73681f580989a1bb99c40532c5464
Ashley Casey <acasey.dev@gmail.com>
2018-01-08 03:33:49 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,1 +1,11 @@
-var playlist = new Object();
+var playlist = new Object({ Artist: 'Mount Kimbie', Song: 'Home Recording' });
+
+function updatePlaylist(playlist, artistName, songTitle) {
+   playlist[artistName] = songTitle
+   return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist;
+}

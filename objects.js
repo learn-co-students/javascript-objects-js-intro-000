ab9001bd9460bc702339ffe520f1c769184ba5c3
Simone D <monatru3@hotmail.com>
2018-01-30 23:00:32 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,12 @@
+var playlist= {"Shakira": "Hips Dont Lie"}
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName]=songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+
+  delete playlist[artistName];
+  return playlist
+}

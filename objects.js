c27feb092f646e2e86b8410955b843a9542eee7a
Ryan <neodynia@icloud.com>
2018-02-18 20:14:50 -0500

    Done.

--- objects.js
+++ objects.js
@@ -1,14 +1,14 @@
 var playlist = {
   artistName: "",
-  songTitle: ""
+  songTitle: "",
 };
 
 function updatePlaylist(playlist,artistName,songTitle) {
   playlist[artistName] = songTitle;
   return playlist;
-}
+};
 
 function removeFromPlaylist(playlist,artistName) {
-  delete playlist.artistName;
-  return playlist;
+  delete playlist.Slowdive; 
+  playlist; 
 }

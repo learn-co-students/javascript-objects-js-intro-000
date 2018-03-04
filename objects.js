afa414a6131114eb2a624adec7d9ca56950c7fcd
Marcus Jun <mjun@calstate.edu>
2018-03-04 04:48:07 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist={artist:"Yellow"}
+
+function updatePlaylist(playlist,artistName,songTitle){
+  playlist[artistName]=songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist,artistName){
+  delete playlist[artistName]
+}

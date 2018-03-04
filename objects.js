6febae73dcb49e243c4e6640f01730ec47360993
Kate Schwarz <kateschwarz0010@gmail.com>
2018-03-04 22:54:27 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,16 @@
+var playlist = {
+  Alison:'Slowdive',
+  Sometimes:'My Bloody Valentine'
+}
+
+function updatePlaylist(playlist,artistName,songTitle)
+{
+  playlist[artistName]=songTitle;
+  return playlist
+}
+
+function removeFromPlaylist(playlist,artistName)
+{
+  delete playlist[artistName]
+  return playlist
+}

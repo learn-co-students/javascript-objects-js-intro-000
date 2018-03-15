a9fa175e26c9297abcacb5f9eb43386338180883
Liangtian Zhang <lzhang40@binghamton.edu>
2018-03-15 03:02:14 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist = {maroon5: 'sunday morning'}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+}

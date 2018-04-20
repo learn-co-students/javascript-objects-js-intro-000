a93a755c830e01a134dc07e6427b72f5cbb86e7e
Shai Goldberg-Kellman <sgoldbergkellman@gmail.com>
2018-04-20 20:37:50 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,7 @@
+var playlist = { taylorSwift:"Love Story"}
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+}
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]}

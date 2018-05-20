42e59be8c789e744159a2270ffb645aacc47e821
William Adu-Krow <mubah125@gmail.com>
2018-05-20 18:57:41 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = {'Phil Ochs': 'Slowdive'};
+
+function updatePlaylist(playlist, artistName, songTitle){
+
+ return Object.assign({} , playlist , { [artistName]: songTitle })
+
+
+};
+
+function removeFromPlaylist(playlist, artistName){
+
+return delete playlist[artistName];
+};

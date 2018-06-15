7a3885596569def5cba76eaf072cd75657cfcba8
Christine Xiang <yutingcxiang@gmail.com>
2018-06-15 00:32:38 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {"Maroon 5":"Animals"};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist;
+}

0d1f803f70e1d14c9562bd8fd77bf0fd92542c53
Robbie Kahl <rob.kahl@target.com>
2018-06-25 18:57:43 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = new object({key: '0'})
+
+function updatePlaylist(playlist, artist name, songTitle) {
+  playist(artistName) = songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+}

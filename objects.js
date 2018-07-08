71133449a0adb6e70f6893a553445ec0edcad720
Cristal Alatorre Perez <cristalcodes@gmail.com>
2018-07-08 21:27:35 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,3 +1,15 @@
 var playlist={
-  johnMayer: 'Free Falling'
+  "Slowdive": 'Alison',
+  "My Bloody Valentine":'Sometimes'
+}
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist['Phil Ochs']='Here\'s to the State of Mississippi'
+
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist["Slowdive"]
+  return playlist
 }

3d480ad2a0306d4f5dc31a870de71895374e44bd
Chris Gabel <cwgabel@yahoo.com>
2018-08-19 16:02:40 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,16 @@
+var playlist = { 'Sinatra' : 'New York, New York'}
+
+
+function updatePlaylist(list, artist, title) {
+  list[artist] = title
+}
+
+function removeFromPlaylist(list, artist) {
+  delete list[artist]
+}
+
+
+
+
+
+//

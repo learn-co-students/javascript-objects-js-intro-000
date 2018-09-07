45b0f7d564c3e505f659b3a481ac2e32b7fc9336
Johnathon Wood <jaywood128@gmail.com>
2018-09-07 22:46:55 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,3 +1,13 @@
 var playlist = {
-  artist name: 'song title'
+  'artist name': 'song title',
+}
+function updatePlaylist(playlist, artist, song) {
+  playlist[artist] = song
+
+  return playlist
+}
+function removeFromPlaylist(playlist, artist) {
+  delete playlist[artist]
+
+  return playlist
 }

af91ab8cc6cc3b2716d458a1dab95086abeefdeb
barry.c.schechter@gmail.com <barry.c.schechter@gmail.com>
2018-09-14 13:41:43 +0000

    Done.

--- objects.js
+++ objects.js
@@ -5,7 +5,7 @@ function updatePlaylist (playlist, artistName, songTitle) {
   playlist[artistName] = songTitle;
   return playlist
 }
-function removeFromPlaylist (playlist, artistName) {
-  delete playlist.artistName;
+function removeFromPlaylist (playlist, artistName){
+  delete playlist[artistName];
   return playlist
 }

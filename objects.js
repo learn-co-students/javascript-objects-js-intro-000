72405ba5c66464cf38a1737bf0e6d4993ee2082a
V <rvalentin2024@gmail.com>
2018-09-28 15:18:11 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = { mike: 'blue'};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+}

5d5c6ac01346dc70be161aa5ee522512d21b57ff
andy reyes <11710reyes@gmail.com>
2018-11-14 20:07:44 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist = { Coldplay: 'The Scientist', Eminem: 'Rap God', Lorde: 'Homemade Dynamite' } 
+
+function updatePlaylist (playlist, artistName, songTitle) {
+  playlist['Slowdive'] = 'Alison';
+  playlist['My Bloody Valentine'] = 'Sometimes';
+  return playlist
+}
+updatePlaylist(playlist, 'Slowdrive', 'Alison');
+updatePlaylist(plalist, 'My Bloody Valentine', 'Sometimes');

f53ee9d53004848b5548daf99e7a1bcc4976049d
Karsten Løvlid <karsten@lovlid.no>
2018-11-19 01:29:49 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+let playlist = {
+  "Johnny": "Be Good"
+};
+
+function updatePlaylist(playlist, artistName, songTitle){
+    return Object.assign(playlist, {[artistName]: songTitle});
+};
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName];
+  return playlist;
+
+};

c111cc221cbb44d83e86e2603d1861d5532c6122
Khaled Elbahr <khaledelbahr2016@gmail.com>
2018-12-01 13:35:04 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = {Adele: 'hello'};
+
+function updatePlaylist (playlist , artistName , songTitle) {
+  return playlist[artistName] = songTitle;
+}
+
+function removeFromPlaylist (playlist , artistName) {
+  delete playlist[artistName];
+  return playlist;
+}

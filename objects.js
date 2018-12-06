fcbadc4c8b2585b7a60e605a854dbe07e422da4a
Alex Schübl <as333@hdm-stuttgart.de>
2018-12-06 20:19:29 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,14 @@
+var playlist = {}
+
+playlist.heleneFischer = "fieber";
+
+function updatePlaylist (playlist, artistName, songTitle){
+   playlist[artistName] = songTitle
+   
+   return playlist
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName]
+  return playlist
+}

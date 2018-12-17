04f3f06b724deaf9beabedb4fc6b9ed0099e9a1b
lanasgoddard <lanasgoddard@gmail.com>
2018-12-17 15:24:40 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,14 @@
+var playlist = {
+Slowdive:'Alison'
+}
+
+function updatePlaylist(playlist, artistName, songTitle){
+    playlist['Phil Ochs'] = 'Heres to the State of Mississippi'
+  playlist['My Bloody Valentine'] = 'Sometimes'
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist.
+  return playlist
+}

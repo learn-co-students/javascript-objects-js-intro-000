5173c8592e35a430bed9f065bf6876e2efa33705
Amandine V <amavelamala@gmail.com>
2019-01-24 02:14:19 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {"The Beatles": "Hey Jude"};
+
+function updatePlaylist(list, name, title) {
+  list[name] = title;
+  return list;
+}
+
+function removeFromPlaylist(list, name) {
+  delete list[name];
+  return list;
+}

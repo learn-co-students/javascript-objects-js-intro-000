e1d0114cc0a261ca9f2712c444eafa79f6640865
Niki Agrawal <nikita24agrawal@gmail.com>
2019-02-10 00:30:46 +0000

    Done.

--- objects.js
+++ objects.js
@@ -5,8 +5,8 @@ var playlist = {
 }
 
 function updatePlaylist(playlist, artistName, songTitle){
-playlist.artistName = songTitle; }
+playlist[artistName] = songTitle; }
 
 function removeFromPlaylist(playlist, artistName){
-  delete playlist.artistName;
+  delete playlist[artistName];
 }

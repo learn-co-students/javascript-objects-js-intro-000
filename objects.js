933a4c4ee377b08bd036fc972cdb6fe73d5ced4d
jocular-markup-2001 <andreacannuni@gmail.com>
2019-02-24 20:19:29 +0000

    Done.

--- objects.js
+++ objects.js
@@ -7,6 +7,6 @@ function updatePlaylist(playlist, key, value) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist.artistName;
+  delete playlist[artistName];
   return playlist;
 }

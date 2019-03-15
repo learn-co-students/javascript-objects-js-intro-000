74c94d25b79781987744bc342da3d1036773a792
emmanuel antoni <emmanuel.antoni@blackrock.com>
2019-03-15 02:43:34 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {artistNames: 'song titles'}
+
+function updatePlaylist (playlist,artistName,songTitle) {
+  return playlist[artistName]='songTitle'
+}
+
+function removeFromPlaylist(playlist,artistName) {
+
+ delete playlist[artistName]
+ return playlist
+  }

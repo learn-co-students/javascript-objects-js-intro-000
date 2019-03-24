7fe3753cb80d210337239418a5fa630e6b14f441
Courtnee M <courtneemeaghan@gmail.com>
2019-03-24 15:44:23 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,14 @@
+var playlist = {
+  ['Lil Baby']: "Rockin",
+  Renae: "Lil dude"
+}
+
+function updatePlaylist(obj, artist, song) {
+  obj[artist] = song;
+  return obj;
+}
+
+function removeFromPlaylist(obj, artist) {
+  delete obj[artist];
+  return obj;
+}

b4792c92dca7aa1a105b22e3fb55e376486505bf
sinduja <sindhu.38@gmail.com>
2019-05-16 03:54:24 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,1 +1,9 @@
-Object.assign({}, playlist, { Rahman: 'Urvasi' })
+var  playlist= { Rahman: 'Urvasi' }
+function updatePlaylist(playlist,artistName,songTitle){
+Object.assign(playlist, playlist, { [artistName]: [songTitle]})
+
+}
+function removeFromPlaylist(playlist,artistName)
+{
+  delete playlist[artistName];
+}

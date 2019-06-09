a82f40fe2b5e77b7535cfc0e3ff3df0dfac25208
Gabriella Ilena <ilenagabriella@gmail.com>
2019-06-09 07:19:57 +0000

    Done.

--- objects.js
+++ objects.js
@@ -5,8 +5,10 @@ var playlist = {
 
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
+  return playlist
 }
 
 function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName]
+  return playlist
 }

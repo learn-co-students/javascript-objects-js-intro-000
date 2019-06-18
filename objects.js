427350a286d98a1ba28d8c94b3beccc2d8f3c19f
Hema Venkata <hvenkata@live.com>
2019-06-18 15:38:24 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,15 @@
+var playlist = {
+  'My Bloody Valentine': "a song",
+  'beyonce': "love on top",
+  'Slowdive': "something",
+  'jonasBros': "sucker"
+};
+
+function updatePlaylist (playlist, artistName, songTitle) {
+  return playlist[artistName] = songTitle
+}
+
+function removeFromPlaylist (playlist, artistName) {
+  delete playlist.artistName;
+  return playlist;
+}

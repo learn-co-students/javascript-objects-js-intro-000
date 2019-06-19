b54a2d3a15415e47b9e4b105a54dc324d64cc22b
Karina Evers <krin_11_bball@yahoo.com>
2019-06-19 20:47:52 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,12 +1,15 @@
 let playlist = {
-  artist: "Avicii",
-  songTitle: "Penguins"
+  'Avicii': 'Penguin' ,
+  'Kygo': 'Firestone'
 };
 
 function updatePlaylist(playlist, artist, songTitle) {
   playlist[artist='Phil Ochs']=[songTitle='My Bloody Valentine']
+  return playlist
 }
+
 
 function removeFromPlaylist(playlist, artist){
   delete playlist[artist='Slowdive'];
+  return playlist
 }

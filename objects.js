53636d60071b79b758b8cf595414bd321746d071
Joaquin Gomez <joaquingomezcabido@gmail.com>
2019-07-06 10:05:10 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = {artist: "song"}
+
+var updatePlaylist = (playlist, artist, song) => {
+  playlist[artist] = song
+
+  return playlist
+}
+
+var removeFromPlaylist = (playlist, artist) => {
+  delete playlist[artist]
+
+  return playlist
+}

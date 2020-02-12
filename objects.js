7d3bb6f2e18048702ee0bce981a0c8885ad6915a
Mary Kang <mary.kang09@gmail.com>
2020-02-12 20:42:19 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = {artistName: "songTitle"}
+
+function updatePlaylist(playlist, artistName, songTitle){
+playlist['Slowdive'] = 'song';
+playlist['My Bloody Valentine'] = 'song2';
+playlist['Phil Ochs'] = 'song3';
+return playlist
+}
+
+function removeFromPlaylist(playlist, artistName){
+delete playlist.Slowdive;
+return playlist;
+}

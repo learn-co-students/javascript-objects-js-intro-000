d66cb23820ea7097928c62ff493bd0210fa8d87e
Ryan Hardin <ryanwhardin@gmail.com>
2020-02-19 14:58:43 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,14 @@
+var playlist = {
+  'My Bloody Valentine': 'Sometimes',
+  'Slowdive': 'Alison'
+}
+
+function updatePlaylist(playlist,artistName,songTitle) {
+  playlist['Phil Ochs']=`Here's to the State of Mississippi`
+  return playlist
+}
+
+function removeFromPlaylist(playlist,artistName) {
+  delete playlist.Slowdive
+  return playlist
+}

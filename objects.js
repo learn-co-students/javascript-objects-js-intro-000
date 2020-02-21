e3ed0eba4e21b398fd37413c3c36efdd64969837
Jackie Sze <jackiesze0@gmail.com>
2020-02-21 23:23:19 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,16 @@
+const playlist = {
+  artist: 'Drake',
+  song: 'Life is Good'
+};
+
+function updatePlaylist(playlist, artist, song) { 
+  playlist[artist] = song
+  return playlist
+  
+  
+}
+
+function removeFromPlaylist(playlist, artist) {
+  delete playlist[artist]
+  return playlist 
+}

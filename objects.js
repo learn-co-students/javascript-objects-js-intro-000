d5d4b159155421cf4f82d8b1102a42b8379cf7e1
Jonathan East <jonathan_east@ymail.com>
2020-02-25 17:24:04 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+const playlist = {
+  "Billy Joel" : "New York State of Mind"
+};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  return Object.assign(playlist, {[artistName]: songTitle});
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+}

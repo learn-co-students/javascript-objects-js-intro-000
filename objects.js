880862ac273ade36014e2d9e8d1a58085991772b
Ravi <kkiran.ror@gmail.com>
2020-03-09 04:29:17 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,1 +1,14 @@
-var object ={}
+var playlist = {}
+playlist['Slowdive'] = 'Alison'
+playlist['My Bloody Valentine'] = 'Sometimes'
+
+function updatePlaylist(playlist, artistName, songTitle){
+  //return Object.assign({}, playlist, { `${artistName}`: songTitle })
+     playlist[`${artistName}`] = songTitle
+     return playlist
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[`${artistName}`]
+  return playlist
+}

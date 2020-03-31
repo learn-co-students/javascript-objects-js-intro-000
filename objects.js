dbe77444a27b88afc353040c15c40025689e54f2
Miao Nan Lin <miaonanlin@gmail.com>
2020-03-31 22:19:59 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist={[name]: 'title'}
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName]
+  return playlist
+}

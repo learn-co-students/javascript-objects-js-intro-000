d395c51e5fc9885be3226473450fb20b5d167d06
kiku ishimoto <kikuish@gmail.com>
2020-04-09 04:28:23 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,15 @@
+var playlist = {
+  indie: 'peach face'
+}
+
+function updatePlaylist(playlist, artist, song) {
+  playlist[artist] = song
+  return playlist
+
+}
+
+function removeFromPlaylist(playlist, artist) {
+  delete playlist[artist];
+
+   return playlist
+}

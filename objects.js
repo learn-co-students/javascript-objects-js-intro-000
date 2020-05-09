462d3c419805c23860b9c707d4c1bb5586587848
Harold Lee <harry.yg.lee@gmail.com>
2020-05-09 16:01:55 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,8 +1,7 @@
 const playlist={artistName:'song'}
-function updatePlaylist(playlist,artistName,song){
-  return playlist[artistName]=song
+function updatePlaylist(playlist, artistName,songTitle){
+  return playlist[artistName]='songTitle'
 }
 function removeFromPlaylist(playlist,artistName){
-  delete playlist[artistName]
-  return playlist
+  return delete playlist[artistName]
 }

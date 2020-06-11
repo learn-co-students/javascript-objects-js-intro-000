50353cb468b42af763b1a80598e13bbf158665a4
Kyle Nugent <kln5656@gmail.com>
2020-06-11 20:48:48 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,4 +1,7 @@
-var playlist = {songTitle: [] };                                // one way to make an Object
+var playlist = {
+  'Foo Fighters': 'Everlong',
+  Prince: 'Purple Rain'
+}                               // one way to make an Object
 
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle                             // add a key and value to object playlist
@@ -6,6 +9,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName) {             // remove a key-value from object called playlist
-  delete playlist[artistName];
+  delete playlist[artistName]
   return playlist
 }

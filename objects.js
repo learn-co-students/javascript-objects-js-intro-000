0101fbc0071b408836d4777decec17ceb838bfee
jingling jin <jljin0524@gmail.com>
2020-06-28 03:36:34 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist = {gdragon:"abc",Lee:"cde"}
+function updatePlaylist(playlist,key,value){
+  playlist[key]=value
+  return playlist
+}
+function removeFromPlaylist(playlist,key){
+  delete playlist[key]
+  return playlist
+}

82819020b815d0c504ea7644a6a900b2a9df860f
reflective-bell-5160 <mvu7@uic.edu>
2020-08-09 20:14:34 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {
+  Taylor: "OMG"
+};
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle;
+};
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName];
+};

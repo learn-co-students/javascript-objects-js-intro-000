325cba79105c005291525552547c612b668010b3
Ada Huang <ada10huang@gmail.com>
2020-08-11 22:47:25 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,9 +1,15 @@
-var playlist = {eltonJohn: "Tiny Dancer"}
-
 var playlist = {"Slowdive": "Alison", "My Bloody Valentine": "Sometimes"};
 
 var updatePlaylist = (playlist, artistName, songTitle) => {
-  return Object.assign({}, playlist, {artistName: songTitle})
+  playlist[artistName] = songTitle
+  return playlist
 }
 
 updatePlaylist(playlist, "Phil Ochs", "Here's to the State of Mississippi")
+
+var removeFromPlaylist = (playlist, artistName) => {
+  delete playlist[artistName];
+  return playlist
+}
+
+removeFromPlaylist(playlist, "Slowdive")

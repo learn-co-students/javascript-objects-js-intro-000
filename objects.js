ae00df1cd9ecd7b782ec595843c00cf8010001ab
Ada Huang <ada10huang@gmail.com>
2020-08-11 21:58:19 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,1 +1,5 @@
-var playlist = {Elton John: "Tiny Dancer"}
+var playlist = {eltonJohn: "Tiny Dancer"}
+
+var updatePlaylist = (playlist, artistName, songTitle) => {
+  return Object.assign({}, playlist, {artistName: [songTitle})
+}

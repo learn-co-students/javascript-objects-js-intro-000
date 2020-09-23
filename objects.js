4057bbfa31649e4b5b90015f7efb8f9a6bfca431
Cole Cezeaux <colecezeaux@gmail.com>
2020-09-23 16:13:46 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,14 +1,13 @@
 var playlist = {
-  Alison : "Slowdive",
-  Sometimes : "My Bloody Valentine"
+  "Slowdive" : "Alison",
+  "My Bloody Valentine" : "Sometimes"
 }
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist['Phil Ochs'] = "Here's to the State of Mississippi"
+  playlist['Phil Ochs'] = 'Here/s to the State of Mississippi'
   return playlist
-  }
+}
 
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist.Slowdive
-  return playlist
+  return delete playlist.Slowdive
 }

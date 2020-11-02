d41c6aaf43a2d1f60dd92f4437a1d65301bfb581
Chris <chris.mahfouz@hotmail.com>
2020-11-02 12:40:55 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist={chris:'good'}
+
+function updatePlaylist(playlist,artistName,songTitle){
+  playlist[artistName] = songTitle
+    return playlist
+}
+function  removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName]
+  return playlist
+}

78bcfb1104c7c465943ec70fefddbf655fcf292b
Adrienne Paquin <adriennepaquin@gmail.com>
2021-03-29 18:52:49 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = { 'Lake Street Dive' : "Side Pony" };
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist;
+}
